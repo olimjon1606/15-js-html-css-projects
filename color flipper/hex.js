1c937026c41e9eab51abe347dc57c8255486aa82
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-23 11:09:03 +0200

    Update hex.js

--- color flipper/hex.js
+++ color flipper/hex.js
@@ -3,17 +3,15 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
 const btn = document.getElementById("btn")
 const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
-    const colorPick = []
+    let hexFinal = "#";
     function randomN() {
         const randomN = Math.floor(Math.random() * 16);
         return randomN
     }
-
     for (i = 0; i < 6; i++) {
-        colorPick.push(hex[randomN()]);
+        hexFinal +=hex[randomN()];
     }
-    const hexFinal = "#" + colorPick.join("")
-    document.body.style.backgroundColor = hexFinal
-    color.textContent = hexFinal
+document.body.style.backgroundColor = hexFinal
+color.textContent = hexFinal
 
 })
