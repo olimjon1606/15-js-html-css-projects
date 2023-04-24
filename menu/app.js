aa0f288b5618d6e7c9aca4b65bc06f65d2a3ef0e
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-24 20:47:59 +0200

    done

--- menu/app.js
+++ menu/app.js
@@ -71,27 +71,80 @@ const menu = [
         img: "./images/item-9.jpeg",
         desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
+    {
+        id: 10,
+        title: "bison steak",
+        category: "dinner",
+        price: 22.99,
+        img: "./images/item-10.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
 ];
-
-const sectionSection = document.querySelector(".section-center")
-
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
 window.addEventListener("DOMContentLoaded", function () {
-    displayMenuItem(menu)
-})
+    diplayMenuItems(menu);
+    displayMenuButtons();
+});
 
-function displayMenuItem(menuItems) {
-    let displayMenu = menu.map(function (item) {
+function diplayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+
         return `<article class="menu-item">
-                    <img src=${item.img} alt="menu item" class="photo">
-                    <div class="item-info">
-                        <header>
-                            <h4>${item.title}</h4>
-                            <h4 class="price">${item.price}</h4>
-                        </header>
-                        <p class="item-text">${item.desc}</p>
-                    </div>
-                </article>`
-    })
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">$${item.price}</h4>
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+          </article>`;
+    });
     displayMenu = displayMenu.join("");
-    sectionSection.innerHTML = displayMenu;
+
+    sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
+    const categories = menu.reduce(
+        function (values, item) {
+            if (!values.includes(item.category)) {
+                values.push(item.category);
+            }
+            return values;
+        },
+        ["all"]
+    );
+    const categoryBtns = categories
+        .map(function (category) {
+            return `<button type="button" class="filter-btn" data-id=${category}>
+            ${category}
+          </button>`;
+        })
+        .join("");
+
+    btnContainer.innerHTML = categoryBtns;
+    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+    console.log(filterBtns);
+
+    filterBtns.forEach(function (btn) {
+        btn.addEventListener("click", function (e) {
+            // console.log(e.currentTarget.dataset);
+            const category = e.currentTarget.dataset.id;
+            const menuCategory = menu.filter(function (menuItem) {
+                // console.log(menuItem.category);
+                if (menuItem.category === category) {
+                    return menuItem;
+                }
+            });
+            if (category === "all") {
+                diplayMenuItems(menu);
+            } else {
+                diplayMenuItems(menuCategory);
+            }
+        });
+    });
 }
