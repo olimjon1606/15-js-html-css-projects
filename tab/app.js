ad881acdff8191d1c4ff921cc9e29e9e76f03c6e
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-27 15:46:02 +0200

    end

--- tab/app.js
+++ tab/app.js
@@ -3,5 +3,17 @@ const about = document.querySelector(".about")
 const articles = document.querySelectorAll(".content")
 
 about.addEventListener("click", (e) => {
-    console.log(e.target.dataset.id)
+    const id = e.target.dataset.id
+    if(id){
+        btns.forEach(function(btn){
+            btn.classList.remove("active")
+            e.target.classList.add("active")
+        })
+
+        articles.forEach(function(article){
+            article.classList.remove("active")
+        })
+        const element = document.getElementById(id)
+        element.classList.add("active")
+    }
 })
