584cad4638013ce7d49502b5d9f9da8159d9d5df
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-28 13:46:47 +0200

    Update app.js

--- lorem gen/app.js
+++ lorem gen/app.js
@@ -15,9 +15,21 @@ const form = document.querySelector(".lorem-form")
 const amount = document.getElementById("amount")
 const result = document.querySelector(".lorem-text")
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", function (e) {
     e.preventDefault()
     // console.log("hello")
-    const value = amount.value
-    console.log(value)
+    const value = parseInt(amount.value)
+    const random = Math.floor(Math.random() *text.length)
+    console.log(typeof (value))
+    if (isNaN(value) || value <= 0 || value > 9) {
+        result.innerHTML = `<p class="result"> ${text[random]}</p>`
+    }
+    else{
+        let tempText = text.slice(0,value)
+        tempText = tempText.map(function(item){
+            return `<p class="result"> ${item}</p>`
+        }).join('')
+        result.innerHTML = tempText
+    }
+
 })
